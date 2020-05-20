e811f0c2f73498aead4ba9affda2f0af76637647
idontunderstandtbh <65286701+idontunderstandtbh@users.noreply.github.com>
2020-05-20 16:10:36 +0300

    Hello
    This is my first file

---
+++ kozlova.py
@@ -0,0 +1,13 @@
+def third_es():
+    word = input ('Введите существительное в единственном числе: \n')
+    plural = word + 'j'
+    print ('Множественное число:',plural)
+    new = word + 'n'
+    print ('Винительный падеж:', new)
+    new = plural + 'n'
+    print ('Винительный во множественном: ',new)
+first_es()
+
+
+
+
